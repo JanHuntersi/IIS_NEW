67f42005eb53c9ce30a0e18129e9e178c1202749
= <jan.sernec@gmail.com>
2024-05-03 19:23:19 +0200

    test api

--- tests/test_apis.py
+++ tests/test_apis.py
@@ -1,11 +1,21 @@
+import requests
 import test_mbajk_api as mbajk_api
 import test_weather_api as weather_api
 import pytest
 
+weater_api_url = "https://api.open-meteo.com/v1/forecast"
 
 def main():
-    mbajk_api()
-    weather_api()
+    contract = "maribor"
+    api_key = "5e150537116dbc1786ce5bec6975a8603286526b"
+    url = f"https://api.jcdecaux.com/vls/v1/stations?contract={contract}&apiKey={api_key}"
+    response = requests.get(url)
+    print(response.status_code)
+    assert response.status_code == 200
+
+    res = requests.get(weater_api_url)
+    print(res.status_code)
+    assert res.status_code == 200
 
 if __name__ == "__main__":
     pytest.main()
